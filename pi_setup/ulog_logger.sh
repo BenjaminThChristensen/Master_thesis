f1465351551b5683a1ba33d28d5a4ce0ba1bf99d
Benjamin Christensen <benjaminthordahlchristensen@gmail.com>
2025-05-19 12:15:16 +0200

    cleanup in pi_setup

--- pi_setup/ulog_logger.sh
+++ pi_setup/ulog_logger.sh
@@ -4,5 +4,6 @@
 source /opt/ros/humble/setup.bash
 export PATH="$HOME/.local/bin:$PATH"
 
-# Start the MAVLink logger
+# Start the ulog logger
 exec python3 /home/dronepi/PX4-Autopilot/Tools/mavlink_ulog_streaming.py /dev/serial0 --baudrate 115200 --output /home/dronepi/flight_logs/ulg
+
